280841dfc0a0047aaa98d74a1c1c25c5073d0b2e
Emilson Silva <emilsongyn17@gmail.com>
2022-12-07 00:13:51 -0300

    Utilizando os métodos GET e POST do HTTP

--- lib/main.dart
+++ lib/main.dart
@@ -36,17 +36,28 @@ class _MyHomePageState extends State<MyHomePage> {
       'content-type': 'application/json',
     };
 
-    http.Response response = await http.get(
+    Map<String, dynamic> body = {
+      'title': 'teste',
+      'body': 'conteudo',
+      'userId': 1,
+    };
+
+    http.Response response = await http.post(
       Uri.parse('https://jsonplaceholder.typicode.com/posts'),
       headers: hearders,
+      body: json.encode(body),
     );
 
-    if (response.statusCode == 200) {
-      List result = json.decode(response.body);
+    //Respostas de sucesso (200-299),
 
-      for (var item in result) {
-        print(item['title']);
-      }
+    if (response.statusCode >= 200 && response.statusCode < 300) {
+      print(response.body);
+
+      // List result = json.decode(response.body);
+
+      // for (var item in result) {
+      //   print(item['title']);
+      // }
     } else {
       print('aconteceu um erro: ${response.statusCode}');
     }
